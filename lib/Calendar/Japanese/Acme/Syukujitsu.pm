0345bb96bc66668d8f5e0dcd82cf62bec572fdb0
oddmutou <n@o625.com>
2017-08-25 10:33:01 +0900

    Modify lib

--- lib/Calendar/Japanese/Acme/Syukujitsu.pm
+++ lib/Calendar/Japanese/Acme/Syukujitsu.pm
@@ -3,139 +3,125 @@ package Calendar::Japanese::Acme::Syukujitsu;
 use 5.006;
 use strict;
 use warnings;
+use utf8;
+
+use Carp qw(croak);
+use Class::Accessor::Lite (ro => [qw(max_year min_year syukujitsus)]);
+use Encode;
+use File::Slurp;
+use Furl;
+use List::Util qw(max min);
+use Smart::Args;
+
+our $VERSION = '0.01';
+our $DEFAULT_ENDPOINT = 'http://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv';
+
+sub new {
+    args
+        my $class,
+        my $cachefile => {optional => 1},
+        my $endpoint => {optional => 1};
+
+    my $data;
+    if ($cachefile) {
+        $data = read_file($cachefile);
+    } else {
+        $endpoint = $endpoint || $DEFAULT_ENDPOINT;
+        $data = Furl->new()->get($endpoint)->content;
+    }
+    Encode::from_to($data, 'sjis', 'utf-8');
+
+    my $syukujitsus = {};
+    for my $line (split /\n/, $data) {
+        chop($line) if $line =~ /(\n|\r|\n\r)$/;
+        my ($date, $syukujitsu_name) = split /,/, $line;
+        my ($year, $month, $day) = split /-/, $date;
+        next unless $day;
+
+        $month =~ s/^0//;
+        $day =~ s/^0//;
+        $syukujitsus->{$year}{$month}{$day} = $syukujitsu_name;
+    }
+
+    bless +{
+        max_year => max(keys %$syukujitsus),
+        min_year => min(keys %$syukujitsus),
+        syukujitsus => $syukujitsus,
+    } => $class
+}
+
+sub get_syukujitsus {
+    args
+        my $self,
+        my $year,
+        my $month => {optional => 1},
+        my $day => {optional => 1};
+
+    croak "$year is too old for Japanese government calendar." if $year < $self->min_year;
+    croak "$year is too new for Japanese government calendar." if $year > $self->max_year;
+
+    if (!$day and !$month) {
+        return $self->syukujitsus->{$year};
+    } elsif (!$day) {
+        return $self->syukujitsus->{$year}{$month};
+    } else {
+        return $self->syukujitsus->{$year}{$month}{$day};
+    }
+}
+
+sub is_syukujitsu {
+    args
+        my $self,
+        my $year,
+        my $month,
+        my $day;
+
+    croak "$year is too old for Japanese government calendar." if $year < $self->min_year;
+    croak "$year is too new for Japanese government calendar." if $year > $self->max_year;
+
+    return $self->syukujitsus->{$year}{$month}{$day};
+}
+
+1;
+
+__END__
 
 =head1 NAME
 
-Calendar::Japanese::Acme::Syukujitsu - The great new Calendar::Japanese::Acme::Syukujitsu!
+Calendar::Japanese::Acme::Syukujitsu - Japanese Syukujitsu in calender
 
 =head1 VERSION
 
 Version 0.01
 
-=cut
-
-our $VERSION = '0.01';
-
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+  use Calendar::Japanese::Acme::Syukujitsu;
 
-Perhaps a little code snippet.
+  # Getting a list of Syukujitsus
+  $holidays = get_syukujitsus(2017, 8);
+  $holidays = get_syukujitsus(2017, 8, 18);
 
-    use Calendar::Japanese::Acme::Syukujitsu;
+  # Examining whether it is holiday or not.
+  $name = is_syukujitsu(2017, 9, 11);
 
-    my $foo = Calendar::Japanese::Acme::Syukujitsu->new();
-    ...
+=head1 DESCRIPTION
 
-=head1 EXPORT
+This module read Syukujitsu information from
+F<syukujitsu.csv> that published by Japanese government.
+With interface that referenced to C<Calendar::Japanese::Holiday>.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head1 METHODS
 
-=head1 SUBROUTINES/METHODS
+=head2 new([cachefile => $cachefile][endpoint => $endpoint])
+Constructor.
 
-=head2 function1
+=head2 get_syukujitsus(year => $year [, month => $month [, day => $day]])
 
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
+=head2 is_syukujitsu(year => $year, month => $month, day => $day)
 
 =head1 AUTHOR
 
-Nao Muto, C<< <n at o625.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-calendar-japanese-acme-syukujitsu at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Calendar-Japanese-Acme-Syukujitsu>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Calendar::Japanese::Acme::Syukujitsu
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Calendar-Japanese-Acme-Syukujitsu>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Calendar-Japanese-Acme-Syukujitsu>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Calendar-Japanese-Acme-Syukujitsu>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Calendar-Japanese-Acme-Syukujitsu/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2017 Nao Muto.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+Nao Muto <n@o625.com>
 
 =cut
-
-1; # End of Calendar::Japanese::Acme::Syukujitsu
